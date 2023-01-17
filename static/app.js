639161bd47131b0a8b1c718872606c9aac7bf39b
aldottexe <aldottexe@gmail.com>
2023-01-17 18:26:12 -0500

    removed old set attribute line

--- static/app.js
+++ static/app.js
@@ -1,8 +1,6 @@
 console.log("gonna connect to the johnsky john")
 const socket = io.connect('http://raspberrypi.local:80')
 
-document.querySelector('p').setAttribute("style", "color:red");
-
 const color1 = document.querySelector('#c1');
 color1.addEventListener('input', () => {
     //console.log("color changed"+input.value);
